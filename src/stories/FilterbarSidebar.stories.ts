0a1af3feff76819a71fde7fd7f7e35c7ccbcc51b
David Leiva <davidleivaherrero@gmail.com>
2023-07-25 23:45:35 +0200

    mini change

--- src/stories/FilterbarSidebar.stories.ts
+++ src/stories/FilterbarSidebar.stories.ts
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Button } from './Button';
 import FilterSideBar from '../components/FilterSideBar';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
